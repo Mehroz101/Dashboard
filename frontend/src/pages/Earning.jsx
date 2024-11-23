cbfafd60bb31d2cd03628def1a28c1816be34502
Mehroz farooq <85057779+Mehroz101@users.noreply.github.com>
2024-11-23 11:15:19 +0500

    Update Earning.jsx

--- frontend/src/pages/Earning.jsx
+++ frontend/src/pages/Earning.jsx
@@ -6,9 +6,9 @@ const Earning = () => {
     <>
       <div className="earning_page">
         <div className="earning_cards flex justify-content-between align-content-center gap-4 flex-wrap">
-          <Earning_card title={"Revenue"} revenue={"3425"} percentage={"10"} />
-          <Earning_card title={"Expense"} revenue={"345"} percentage={"15"} />
-          <Earning_card title={"Profit"} revenue={"2768"} percentage={"26"} />
+          <Earning_card title={"Revenue"} revenue={"3425"} percentage={"10"} icon={"pi pi-credit-card"} />
+          <Earning_card title={"Expense"} revenue={"345"} percentage={"15"} icon={"pi pi-money-bill"} />
+          <Earning_card title={"Profit"} revenue={"2768"} percentage={"26"} icon={"pi pi-dollar"} />
         </div>
 
         <div className="p-4 bg-gray-100 rounded-lg shadow-md">
